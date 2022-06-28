10e8a582e4081f6d4539e013fe71097cabed30af
gerald-haywood <ghaywood93.gh@gmail.com>
2022-06-27 22:09:44 -0700

    Search bar is now being displayed

--- src/Components/search/Search.js
+++ src/Components/search/Search.js
@@ -3,8 +3,17 @@ import React, { Component } from 'react';
 class Search extends Component {
     render(){
         return(
-            <div>
-                Searchhhhh
+            <div className="search">
+                <form>
+                    <label>
+                        <input
+                            type="serach"
+                            ref="username"
+                            placeholder="Enter a username and hit enter."
+                        >
+                        </input>
+                    </label>
+                </form>
             </div>
         );
     }
