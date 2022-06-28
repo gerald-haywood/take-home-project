3193edbbb77bd44b35feaf58c4433976b041e0ed
gerald-haywood <ghaywood93.gh@gmail.com>
2022-06-27 22:02:43 -0700

    Fetches data from Github API

--- src/Components/github/Github.js
+++ src/Components/github/Github.js
@@ -4,10 +4,52 @@ import Search from './Components/search/Search';
 
 const API = 'https://api.github.com/users';
 class Github extends Component {
+
+    constructor(props) {
+      super(props)
+    
+      this.state = {
+         username: 'hiteshchoudhary',
+         name: '',
+         avatar: '',
+         repos: '',
+         followers: '',
+         following: '',
+         homeURL: '',
+         notFound: ''
+      };
+    }
+
+    getProfile(username){
+        let finalURL = `${API}/${username}`;
+        fetch(finalURL)
+        .then((res) => res.json())
+        .then((data) => {
+            this.setState({
+                username: data.login,
+                name: data.name,
+                avatar: data.avatar,
+                repos: data.repos,
+                followers: data.followers,
+                following: data.following,
+                homeURL: data.homeURL,
+                notFound: data.message
+            })
+        })
+        .catch((error) => console.log('Unable to fetch data'))
+    }
+
+    componentDidMount(){
+        this.getProfile(this.state.username);
+    }
+
     render(){
         return(
             <div>
-                Github Info here
+                <section id="card">
+                    <Search />
+                    <Profile />
+                </section>
             </div>
         );
     }
