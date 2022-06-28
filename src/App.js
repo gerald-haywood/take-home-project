ec323203938b5e092fef0a945723df75e4a047eb
gerald-haywood <ghaywood93.gh@gmail.com>
2022-06-27 21:37:37 -0700

    Added navbar to top of web app, will edit design later

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import './App.css';
 import React from 'react';
 import Header from './Components/header/Header';
+// import Github from './Components/github/Github';
+import './css/bootstrap.min.css';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      {/* <Github /> */}
       <h1> Hello there </h1>
     </div>
   );
---
+++ src/Components/github/Github.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+// import 
+
+class Github extends Component {
+    render(){
+        return(
+            <div>
+                Github Info here
+            </div>
+        );
+    }
+}
+
+export default Github;
