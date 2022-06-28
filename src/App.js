85b1292e6a0c3be0c4d8bde084c6a4ac8534e263
gerald-haywood <ghaywood93.gh@gmail.com>
2022-06-27 22:08:46 -0700

    Imported the Search component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import React from 'react';
 import Header from './Components/header/Header';
+import Search from './Components/search/Search';
 import './css/bootstrap.min.css';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Search />
     </div>
   );
 }
