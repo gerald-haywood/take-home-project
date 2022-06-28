9ad692fdf7b6b177f042cee14d8b1aa01998f986
gerald-haywood <ghaywood93.gh@gmail.com>
2022-06-28 13:16:38 -0700

    Removed unneccessary code

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import './App.css';
 import React from 'react';
 import Header from './Components/header/Header';
-import Search from './Components/search/Search';
 import './css/bootstrap.min.css';
 import Github from './Components/github/Github';
 
