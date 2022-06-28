8575a10bce5ddbf27a7357c8170968ce88337ba7
gerald-haywood <ghaywood93.gh@gmail.com>
2022-06-28 13:10:19 -0700

    Simplified header

--- src/Components/header/Header.js
+++ src/Components/header/Header.js
@@ -3,28 +3,9 @@ import React, { Component } from 'react';
 class Header extends Component {
     render() {
         return (
-            <nav class="navbar navbar-expand-lg bg-light">
-  <div class="container-fluid">
-    <a class="navbar-brand" href="#">Navbar</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarNav">
-      <ul class="navbar-nav">
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#">Home</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">One</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Two</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link disabled">Three</a>
-        </li>
-      </ul>
-    </div>
+            <nav className="navbar navbar-expand-lg bg-light">
+  <div className="container-fluid">
+    <div className="navbar-brand">Github User Directory</div>
   </div>
 </nav>
         );
