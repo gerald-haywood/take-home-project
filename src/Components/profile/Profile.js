7f22b6436d2b92dd02088fadaa18ac6732d4eb1e
gerald-haywood <ghaywood93.gh@gmail.com>
2022-06-28 13:15:50 -0700

    Updated if-else statement to if-elseif-else to account for both unknown and incorrect usernames

--- src/Components/profile/Profile.js
+++ src/Components/profile/Profile.js
@@ -4,70 +4,56 @@ class Profile extends Component {
 
     render(){
         let userdata = this.props.userData;
-        let followers = `${userdata.homeURL}/followers`;
-        let following = `${userdata.homeURL}/following`;
-        let repos = `${userdata.homeURL}/repositories`;
-        
-        if(userdata.notFound === "User not found"){
+
+        if(userdata.name === null) {
             return(
-                <div className='notFound'>
-                    <h2>Hey!</h2>
-                    <p>Profile not found. Please try again.</p>
+                <div>
+                    Please enter a username.
                 </div>
             );
         }
 
+        else if(!userdata.name) {
+            return(
+                <div>
+                    Please enter a valid username.
+                </div>
+            )
+        }
         else {
             return(
                 <section>
                     <div className="github-profile">
-                        <a
-                            href={userdata.homeURL}
-                            target="_blank"
-                            title={userdata.name || userdata.username}
-                        >
-                            <img src={userdata.avatar} />
-                        </a>
+                        <div title={userdata.username}>
+                            <img src={userdata.avatar} alt="user" />
+                        </div>
                         <h2>
-                            <a
-                                href={userdata.homeURL}
-                                target="_blank"
-                                title={userdata.name || userdata.username}
-                            >
-                                <img src={userdata.avatar} />
-                            </a>
+                            <div title={userdata.username}>
+                                {userdata.name || userdata.username}
+                            </div>
                         </h2>
                         <h3>{userdata.location}</h3>
                     </div>
                     <div className="github-profile-state">
                         <ul>
-                            <li>
-                                <a
-                                    href={followers}
-                                    target="_blank"
-                                    title="Number of followers">
-                                        <i>{userdata.followers}</i>
-                                        <span>Followers</span>
-                                </a>
-                            </li>
-                            <li>
-                                <a
-                                    href={repos}
-                                    target="_blank"
-                                    title="Number of repositories">
-                                        <i>{userdata.repos}</i>
-                                        <span>Repositories</span>
-                                </a>
-                            </li>
-                            <li>
-                                <a
-                                    href={following}
-                                    target="_blank"
-                                    title="Number of users following">
-                                        <i>{userdata.following}</i>
-                                        <span>Following</span>
-                                </a>
-                            </li>
+                            <div
+                                title="Number of followers">
+                                    <span>Number of followers: </span>
+                                    {userdata.followers}
+                                    
+                            </div>
+                            <div
+                                title="Number of repositories">
+                                    <span>Number of repositories: </span>
+                                    {userdata.repos}
+                                    
+                            </div>
+                            <div
+                                title="Number of users following">
+                                    <span>Number of users following: </span>
+                                    {userdata.following}
+                                    
+                            </div>
                         </ul>
                     </div>
                 </section>
