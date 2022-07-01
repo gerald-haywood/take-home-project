3ebee3e64df06dbcba63f2c8f45812c168508f85
gerald-haywood <ghaywood93.gh@gmail.com>
2022-07-01 08:41:58 -0700

    Added another condition to if-statement to show error message if nonexistent username is search, set photo of searched user to one size

--- src/Components/profile/Profile.js
+++ src/Components/profile/Profile.js
@@ -7,7 +7,7 @@ class Profile extends Component {
 
         if(userdata.name === null) {
             return(
-                <div>
+                <div className="text-center">
                     Please enter a username.
                 </div>
             );
@@ -15,16 +15,16 @@ class Profile extends Component {
 
         else if(!userdata.name) {
             return(
-                <div>
+                <div className="text-danger text-center">
                     Please enter a valid username.
                 </div>
             )
         }
         else {
             return(
-                <div className='container-md'>
+                <div className='container-md card-body text-center'>
                     <img src={userdata.avatar} alt="user" className='w-25 p-3 mx-auto' />
-                    <div className='card-body text-center'>
+                    <div>
                         <h4>{userdata.name || userdata.username}</h4>
                         <div className='user-followers'>Followers: {userdata.followers}</div>
                         <div className='user-repositories'>Repositories: {userdata.repos}</div>
